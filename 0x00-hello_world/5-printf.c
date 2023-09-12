dc2dc6b9d583dd14e4405993fb2cdcdc014f9b63
Demas <demasmongare@gmail.com>
2023-09-12 16:03:27 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,12 @@
 
 /**
  * main - Entry point
- * Return - Always 0 (Success)
+ * Return: - Always 0 (Success)
  */
 int main(void)
 {
+	/* sends formatted output to stdout */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
