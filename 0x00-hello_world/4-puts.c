7de5253f3e04efed814734f4468b248069ff3902
Demas <demasmongare@gmail.com>
2023-09-12 15:42:46 -0700

    Printing using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* string initialisation */
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(mystr);
+
+	return (0);
+}
