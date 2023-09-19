d488b886e85ba479c97bdb51851820fef790e4e5
Demas <demasmongare@gmail.com>
2023-09-19 15:38:43 -0700

    Changed logic

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,13 @@ void print_rev(char *s)
 {
 	int i;
 
-	i = 0;
-	while (s[i] == '\n')
+	if (s[i] == '\0')
 	{
-		_putchar(s[i]);
-		i--;
+		while (s[i] >= 0)
+		{
+			_putchar(s[i]);
+			i--;
+		}
 	}
 	_putchar('\n');
 }
