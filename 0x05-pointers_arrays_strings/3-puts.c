0142a9f1acbf30488646a3c5bf80fff34e7e7268
Demas <demasmongare@gmail.com>
2023-09-19 19:44:54 -0700

    Prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - function prints a string, followed by a new line to stdout.
- * @str: function parameter
- *
- * Return: nothing
- */
+* _puts - function prints a string, followed by a new line to stdout.
+* @str: function parameter
+*
+* Return: nothing
+*/
 void _puts(char *str)
 {
 	int i;
 
-	i = 0;
-
-	for (i = 0; i < str[i])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != '\0')
-		{
-			_putchar(str[i]);
-		}
-		i++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -6,6 +6,7 @@
  *
  *Return: on succes 1.
  * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
