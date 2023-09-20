ca01a8850068ae2bd0e5638dc1c761243e6e2b2e
Demas <demasmongare@gmail.com>
2023-09-19 19:56:18 -0700

    Prints a string in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,12 @@ void print_rev(char *s)
 {
 	int i;
 
-	if (s[i] == EOF)
-	{
-		while (s[i] >= 0)
-		{
-			_putchar(s[i]);
-			i--;
-		}
-	}
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	for (i = count - 1 ; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
