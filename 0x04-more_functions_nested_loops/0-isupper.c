ce0b8b3ab24a6807f324db3f5bc5c63b85650094
demasmongare <demasmongare@gmail.com>
2023-11-10 04:44:22 -0800

    1. isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _isupper - function checks for uppercase character
- * @c: The character to be checked
- *
- * Return: Returns 1 if c is uppercase, 0 otherwise
- */
-
+* _isupper - Function checks for uppercase character
+* @c: The character to be checked
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
-    return (1);
-  else
-    return (0);
+if (c >= 'A' && c <= 'z')
+return (1);
+else
+return (0);
 }
